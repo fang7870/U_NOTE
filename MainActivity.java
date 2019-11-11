8da8d7a2db9c2a707f806fda9ae05b282339b233
fang7870 <56316723+fang7870@users.noreply.github.com>
2019-11-11 15:05:46 -0500

    Android studio Main page, about page
    This include a part of start page which have not yet be done, for the main page, there is only one button, another button should be added and link it into the next page which is camera. 
    Question: Now, I am not sure about should we only do "take a photo" or we should do "take a photo" and "choose picture from the phone"

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void openAboutActivity(View view){
+        Intent intent = new Intent (this, AboutActivity.class);
+        startActivity(intent);
+    }
+}
